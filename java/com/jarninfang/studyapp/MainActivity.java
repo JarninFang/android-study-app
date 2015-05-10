02650bf0983e45d7956c7368ad04e00ac06ba746
JarninFang <jarninfang@gmail.com>
2015-05-09 17:41:33 -0700

    Added MainActivity

--- java/com/jarninfang/studyapp/MainActivity.java
+++ java/com/jarninfang/studyapp/MainActivity.java
@@ -1,7 +1,24 @@
 package com.jarninfang.studyapp;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
 /**
  * Created by jarnin on 5/9/15.
  */
-public class MainActivity {
+public class MainActivity extends FragmentActivity {
+
+    /*@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate();
+
+        FragmentManager fm = getFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.mainFragmentContainer);
+
+        if(fragment == null) {
+            fragment = new
+        }
+    }*/
 }
